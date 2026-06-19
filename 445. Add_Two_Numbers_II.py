2dc0b88addd99fb752001f880dcd1fa77652b6b4
Aswin15022006 <aswinbala2k@gmail.com>
2026-06-19 14:15:43 +0530

    Create 445. Add_Two_Numbers_II.py

---
+++ 445. Add_Two_Numbers_II.py
@@ -0,0 +1,28 @@
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head=l1
+        count=""
+        while head!=None:
+            count+=str(head.val)
+            head=head.next
+        count=int(count[0::])
+        tail=l2
+        count1=""
+        while tail!=None:
+            count1+=str(tail.val)
+            tail=tail.next
+        count1=int(count1[0::])
+        add=count+count1
+        newlist=None
+        for i in str(add):
+            if newlist==None:
+                newlist=ListNode(int(i))
+                head=newlist
+            else:
+                n1=ListNode(int(i))
+                newlist.next=n1
+                newlist=n1
+        return head                 
+    
+           
+        
